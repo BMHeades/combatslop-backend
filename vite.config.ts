97db98112242fe4c639461d7dfbe1da4b1654ca4
bmheades <bmheades@gmail.com>
2026-06-24 00:04:06 -0700

    chore: csrf

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,13 @@ export default defineConfig({
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter(),
-
+			csrf: {
+				checkOrigin: false
+				// trustedOrigins: [
+				// 	'chrome-extension://pbacojemoddcchhbabbecnpjmmpalidb',
+				// 	'moz-extension://your_firefox_extension_id_here'
+				// ]
+			},
 			typescript: {
 				config: (config) => ({
 					...config,
